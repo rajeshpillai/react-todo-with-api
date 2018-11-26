afc02aeb8045e60138a9a035607db9ab9255a512
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-26 11:54:15 +0530

    Added api calls for fetching and posting todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const url = "https://jsonplaceholder.typicode.com/todos";
 class App extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.state = {
+      newTodo: "",
+      tasks: []
+    }
+  }
+
+  addTodo() {
+    let newTodo = {
+      userId: 1,  // hardcoded user
+      id: +new Date(),  // assign unique id, numeric date (quick fix)
+      title: this.state.newTodo, // grab newtodo from state
+      completed: false
+    }
+    fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin,
+      body: JSON.stringify(newTodo)
+    }).then(response => {
+      console.log(response.json());
+      // On success update state
+      this.setState({
+        tasks: [newTodo, ...this.state.tasks]
+      })
+    }); // parses response to JSON
+  }
+
+  handleChange(e) {
+    this.setState({
+      newTodo: e.target.value
+    })
+  }
+  componentDidMount() {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          tasks: data
+        })
+      })
+  }
+
   render() {
+    let taskList = this.state.tasks.map((task) => {
+      return (
+        <li key={task.id}>{task.title}</li>
+      )
+    })
     return (
       <div className="App">
+        <h2>Task App</h2>
+        <input onChange={this.handleChange}
+          className="todo-input" type="text"
+          placeholder="what do you want to do today?"
+          value={this.state.newTodo} />
+
+        <button onClick={this.addTodo}>new todo</button>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ul>
+            {taskList}
+          </ul>
         </header>
       </div>
     );
